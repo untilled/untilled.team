4aa2ff3233e6914cb776abe5efad62b0d3a71099
morethanmin <mini4614@gmail.com>
2022-02-25 10:29:24 +0900

    [fix] handle resize

--- src/components/_shared/FullPage/index.tsx
+++ src/components/_shared/FullPage/index.tsx
@@ -20,42 +20,60 @@ const FullPage = ({ page, children, onNext, onPrev }: Props) => {
     [page, heightList, onNext, onPrev]
   )
 
-  const handleTouch = (e: any) => {
-    console.log(e)
-  }
-
-  useEffect(() => {
-    document.addEventListener('mousewheel', handleScroll)
-    // document.addEventListener('touchmove', handleTouch)
-    return () => {
-      document.removeEventListener('mousewheel', handleScroll)
-      // document.removeEventListener('touchmove', handleTouch)
-    }
-  }, [page, handleScroll])
-
-  useEffect(() => {
+  //페이지 변경시 스크룰 이동해야하는 y값 정의
+  const initHeightList = (): number[] => {
     if (wrapperRef.current) {
       const nodes = wrapperRef.current.childNodes
       const nodeHeights: number[] = Array.from(nodes).map(
         (node: any, idx: number) => {
           if (nodes.length - 1 === idx) {
             const prevNode: any = nodes[idx - 1]
-            return prevNode.getBoundingClientRect().y + node.offsetHeight
+            return prevNode.offsetTop + node.offsetHeight
           }
-          const nodeHeight = node.getBoundingClientRect().y
+          const nodeHeight = node.offsetTop
           return nodeHeight
         }
       )
       setHeightList(nodeHeights)
+      return nodeHeights
     }
-  }, [wrapperRef])
+    return []
+  }
+
+  //페이지 높이값 변경시 높이 값 다시 받아온 뒤, 해당 페이지 높이값으로 이동
+  const handleResize = () => {
+    const hightList = initHeightList()
+    if (wrapperRef.current) {
+      const parentNode = wrapperRef.current
+      console.log(hightList, page)
+
+      parentNode.scrollTo(0, hightList[page])
+    }
+  }
+
+  useEffect(() => {}, [page])
 
   useEffect(() => {
+    window.addEventListener('resize', handleResize)
     if (wrapperRef.current) {
       const parentNode = wrapperRef.current
       parentNode.scrollTo(0, heightList[page])
     }
-  }, [page, heightList])
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [page])
+
+  useEffect(() => {
+    window.addEventListener('mousewheel', handleScroll)
+    return () => {
+      window.removeEventListener('mousewheel', handleScroll)
+    }
+  }, [page, handleScroll])
+
+  useEffect(() => {
+    initHeightList()
+  }, [wrapperRef])
 
   return <Wrapper ref={wrapperRef}>{children}</Wrapper>
 }
