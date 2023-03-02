e7b6f898e7d8ed1ab6c80178ef7e367804118390
front_이상민 <lee.sangmin@cashwalk.io>
2023-03-02 17:29:01 +0900

    feat: update fullpage component

--- src/containers/home/components/FullPage/index.tsx
+++ src/containers/home/components/FullPage/index.tsx
@@ -17,54 +17,27 @@ const FullPage: React.FC<FullPageProps> = ({
   onPrev,
 }) => {
   const [heightList, setHeightList] = useState<number[]>([])
-  const [nodes, setNodes] = useState<any>([])
+  const [loading, setLoading] = useState(false)
   const wrapperRef = useRef<HTMLDivElement>(null)
   const isMobile = useMediaQuery(`(max-width: ${breakpoints[0]}px)`)
 
-  // const isMobile = useMediaQuery({
-  //   query: '(max-width: 1024px)',
-  // })
+  const handleScroll = (e: WheelEvent) => {
+    e.preventDefault()
+    if (Math.abs(e.deltaY) < 30) return
+    if (loading) return false
 
-  const handleScroll = useCallback(
-    (e: WheelEvent) => {
-      if (!isMobile) e.preventDefault()
-      //page null 예외처리
-      if (page === null) return
+    if (e.deltaY > 0) {
+      onNext()
+    } else {
+      onPrev()
+    }
+  }
 
-      // 스크룰 강도가 30보다 작은 경우 리턴
-      if (Math.abs(e.deltaY) < 30) return
+  const handleKeyPress = (e: KeyboardEvent) => {
+    if (!isMobile && (e.key === 'ArrowUp' || e.key === 'ArrowDown')) {
+      e.preventDefault()
+      if (loading) return
 
-      // 현재 스크룰된 값과 높이값이 다른 경우 리턴
-      const currentScrollTop = wrapperRef.current?.scrollTop
-        ? Math.floor(wrapperRef.current?.scrollTop)
-        : 0
-
-      if (
-        currentScrollTop - 10 > heightList[page] ||
-        heightList[page] > currentScrollTop + 10
-      )
-        return
-
-      //아래로 이동
-      if (e.deltaY > 0) {
-        onNext()
-      }
-      //위로 이동
-      else {
-        onPrev()
-      }
-    },
-    [page, heightList, nodes, onNext, onPrev]
-  )
-
-  const handleKeyPress = useCallback(
-    (e: KeyboardEvent) => {
-      if (!isMobile && (e.key === 'ArrowUp' || e.key === 'ArrowDown'))
-        e.preventDefault()
-      //page null 예외처리
-      if (page === null) return
-
-      if (wrapperRef.current?.scrollTop !== heightList[page]) return
       switch (e.key) {
         case 'ArrowUp':
           onPrev()
@@ -73,11 +46,9 @@ const FullPage: React.FC<FullPageProps> = ({
           onNext()
           break
       }
-    },
-    [page, heightList, onNext, onPrev]
-  )
+    }
+  }
 
-  // 페이지 변경시 스크룰 이동해야하는 y값 정의
   const initHeightList = (): number[] => {
     if (wrapperRef.current) {
       const nodes = wrapperRef.current.childNodes
@@ -92,14 +63,13 @@ const FullPage: React.FC<FullPageProps> = ({
         }
       )
       setHeightList(heights)
-      setNodes(nodes)
       return heights
     }
     return []
   }
 
   // 페이지 높이값 변경시 높이 값 다시 받아온 뒤, 해당 페이지 위치로 이동
-  const handleResize = useCallback(() => {
+  const handleResize = () => {
     //page null 예외처리
     if (page === null) return
 
@@ -108,52 +78,79 @@ const FullPage: React.FC<FullPageProps> = ({
       const parentNode = wrapperRef.current
       parentNode.scrollTo(0, hightList[page])
     }
-  }, [page])
+  }
 
-  // 페이지 크기 변경시 이벤트 등록
   useEffect(() => {
-    //page null 예외처리
     if (page === null) return
-
-    if (isMobile) {
-      window.removeEventListener('resize', handleResize)
-      return
-    }
-    window.addEventListener('resize', handleResize)
-    if (wrapperRef.current) {
-      const parentNode = wrapperRef.current
-      parentNode.scrollTo(0, heightList[page])
-    }
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [page, heightList, handleResize, isMobile])
+    if (!wrapperRef.current) return
+    animatedScrollTo(heightList[page], 500, () => {})
+  }, [page])
 
   // 페이지 이동 이벤트 등록
   useEffect(() => {
-    if (isMobile) {
+    if (isMobile || page === null) {
+      window.removeEventListener('resize', handleResize)
       window.removeEventListener('wheel', handleScroll)
       window.removeEventListener('keydown', handleKeyPress)
       return
     }
+    window.addEventListener('resize', handleResize)
     window.addEventListener('wheel', handleScroll, { passive: false })
     window.addEventListener('keydown', handleKeyPress, { passive: false })
     return () => {
+      window.removeEventListener('resize', handleResize)
       window.removeEventListener('wheel', handleScroll)
       window.removeEventListener('keydown', handleKeyPress)
     }
-  }, [page, handleScroll, handleKeyPress, isMobile])
+  }, [loading, isMobile])
 
   // 해당 컴포넌트 생성시 높이 초기화
   useEffect(() => {
     initHeightList()
   }, [wrapperRef])
 
+  function animatedScrollTo(
+    scrollTo: number,
+    duration: number,
+    callback: () => void
+  ) {
+    if (!wrapperRef.current) return
+    setLoading(true)
+    const parentNode = wrapperRef.current
+    const scrollFrom = parentNode.scrollTop || 0
+    const scrollDiff = scrollTo - scrollFrom
+    let currentTime = 0
+    const increment = 5
+
+    function animateScroll() {
+      currentTime += increment
+
+      const newScrollPos = easeInOutCubic(
+        currentTime,
+        scrollFrom,
+        scrollDiff,
+        duration
+      )
+
+      parentNode.scrollTo(0, newScrollPos)
+      if (currentTime > duration) {
+        parentNode.scrollTo(0, scrollTo)
+        setLoading(false)
+        callback()
+        return
+      }
+
+      setTimeout(animateScroll, increment)
+    }
+
+    animateScroll()
+  }
+
   return <Wrapper ref={wrapperRef}>{children}</Wrapper>
 }
 
 const Wrapper = styled.div`
-  scroll-behavior: smooth;
+  /* scroll-behavior: smooth; */
   overflow-y: scroll;
   position: fixed;
   top: 0;
@@ -169,3 +166,14 @@ const Wrapper = styled.div`
 `
 
 export default FullPage
+
+function easeInOutCubic(
+  currentTime: number,
+  startValue: number,
+  changeInValue: number,
+  duration: number
+) {
+  const time = currentTime / duration - 1
+  const timeCubic = time * time * time
+  return changeInValue * (timeCubic + 1) + startValue
+}
